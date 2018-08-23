e3e98486f90347901546dc9028af423dd2d99211
oleg-169900 <oleg-169900@yandex.ru>
2018-08-23 21:04:45 +0300

    написал стили для team

--- js/script.js
+++ js/script.js
@@ -7,8 +7,18 @@
   var toggleFeature2 = document.querySelector('#features-toggle-2');
   var toggleFeature3 = document.querySelector('#features-toggle-3');
 
+  var team = document.querySelector('.team__center');
+  var teamSlider = document.querySelector('.team__list');
+  var toggleTeam1 = document.querySelector('#team-toggle-1');
+  var toggleTeam2 = document.querySelector('#team-toggle-2');
+  var toggleTeam3 = document.querySelector('#team-toggle-3');
+
   if (features.classList.contains('features__center--nojs')) {
       features.classList.remove('features__center--nojs');
+  }
+
+  if (team.classList.contains('team__center--nojs')) {
+      team.classList.remove('team__center--nojs');
   }
 
   toggleFeature1.onclick = function() {
@@ -30,5 +40,26 @@
     toggleFeature2.classList.remove('features__toggle--active');
     toggleFeature3.classList.add('features__toggle--active');
     featuresSlider.style.left = '-200%';
+  };
+
+  toggleTeam1.onclick = function() {
+    toggleTeam2.classList.remove('team__toggle--active');
+    toggleTeam3.classList.remove('team__toggle--active');
+    toggleTeam1.classList.add('team__toggle--active');
+    teamSlider.style.left = 0;
+  };
+
+  toggleTeam2.onclick = function() {
+    toggleTeam1.classList.remove('team__toggle--active');
+    toggleTeam3.classList.remove('team__toggle--active');
+    toggleTeam2.classList.add('team__toggle--active');
+    teamSlider.style.left = '-100%';
+  };
+
+  toggleTeam3.onclick = function() {
+    toggleTeam1.classList.remove('team__toggle--active');
+    toggleTeam2.classList.remove('team__toggle--active');
+    toggleTeam3.classList.add('team__toggle--active');
+    teamSlider.style.left = '-200%';
   };
 })();
